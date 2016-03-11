e5e166327c64187076d9b3cc7fa81597d6921997
wx91 <766221790@qq.com>
2016-03-11 11:04:41 +0800

    删除

--- UICollectionViewDemo3/iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/wangx/GitHub/IOSTest/UICollectionViewDemo3/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/wangx/GitHub/IOSTest/UICollectionViewDemo3/iOSInjectionProject/BundleContents.h
